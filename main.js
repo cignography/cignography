d9186153bdf3743a5ea3310498564bccc892459d
cignography <123817189+cignography@users.noreply.github.com>
2023-01-30 15:19:02 +0900

    ver.230130

--- main.js
+++ main.js
@@ -5,3 +5,19 @@ function fetchPage(name){
         })
     });
 }
+
+const tg_btn = document.querySelector('.navbar-btn');
+const menu = document.querySelector('.navbar-menu');
+const logo = document.querySelector('.navbar-logo');
+
+tg_btn.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});
+
+logo.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});

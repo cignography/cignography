509f548b480e913a0b1c5f6e20b595abb6934020
Sunghoon Kim <sunghoonkim010216@gmail.com>
2023-02-09 06:57:22 +0900

    ver.230209

--- photo.js
+++ photo.js
@@ -1,31 +1,52 @@
-window.addEventListener("scroll", loadMore)
+window.addEventListener("scroll", loadMore);
+
+var cnt = 0;
 
 // 난수생성 함수
 function generateRandomNumber() {
-    return Math.floor(Math.random() * 500) + 1;
+    // return Math.floor(Math.random() * 100) + 1;
+    if (cnt == 100){
+        cnt = 0;
+    }
+
+    cnt += 1;
+
+    return cnt;
 }
 
 // 사진 더 불러오기 (test)
 
-function loadMore(){
-    // try{
-        const photo_gallery = document.querySelector('.photo-gallery');
-    // } catch(e){}
+function loadMore() {
+    const photo_gallery = document.querySelector(".photo-gallery");
 
-    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {        
-        for (let i = 0; i < 4; i++) {
+    if (window.innerHeight + window.scrollY + 200 >= document.body.offsetHeight) {
+        for (let i = 0; i < 8; i++) {
             var num = generateRandomNumber();
             console.log(num);
 
-            let newImageDiv = document.createElement('div');
-            newImageDiv.setAttribute('class', 'photo');
-            
-            let newImage = document.createElement('img');
+            let newImageDiv = document.createElement("div");
+            newImageDiv.setAttribute("class", "photo");
+
+            let newImage = document.createElement("img");
             newImageDiv.appendChild(newImage);
-    
-            newImage.src = '/result/output_'+num+'.jpeg';
+
+            newImage.src = "https://cdn.jsdelivr.net/gh/cignography/cignography.github.io@main/photos/" + num + ".jpeg";
 
             photo_gallery.appendChild(newImageDiv);
         }
-    };
+    }  
+    
 }
+
+// var msnry = new Masonry(".photo-gallery", {
+//     itemSelector: ".photo",
+//     // columnWidth: 200
+//     columnWidth: ".sizer",
+//     percentPosition: true,
+//     gutter: 20,
+//     horizontalOrder: true
+// });
+
+// imagesLoaded(".photo-gallery").on("progress", function () {
+//     msnry.layout();
+// });
